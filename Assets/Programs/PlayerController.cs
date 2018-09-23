328ca80508268912b7438e466d05395f15d88432
Lucas Albano de Oliveira <lucasalbao@gmail.com>
2018-09-23 16:03:51 -0300

    Comentários e iniciando a construção de  um Player de efeitos sonoros

--- Assets/Programs/PlayerController.cs
+++ Assets/Programs/PlayerController.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    private float PercentageMoved;
-    private float IniAngle;
-    private float Counter;
-    private float CurrentSpeed;
-    private CharacterController Controller;
-    private bool SeenFriend;
+    private float PercentageMoved;                  //Qual a porcentagem de terreno deslocado pelo jogador
+    private float IniAngle;                         //Angulo inicial da luz do jogador
+    private float Counter;                          //Temporizador
+    private float CurrentSpeed;                     //Velocidade atual atribuida ao jogador
+    private CharacterController Controller;         
+    private bool SeenFriend;                        //Booleana que controla se o jogador está "vendo" o item
     public GameObject Light;
     public GameObject Camera;
-    public float Timer;
-    public float Speed;
-    public float FriendCounter;
-    public float Range;
-    public float Xlimite;
-    public float Zlimite;
-
-	// Use this for initialization
-	void Start () {
+    public float Timer;                             //Tempo que a luz do jogador aumentará após coletar um item
+    public float Speed;                             //A velocidade do jogador
+    public float FriendCounter;                     //Quantos itens o jogador colecionou até agora
+    public float Range;                             //A distância em que o jogador consegue identificar um novo item
+    public float Xlimite;                           //Os limite laterais em que o jogador pode se locomover
+    public float Zlimite;                           //Os limite longitudinais em que o jogador pode se locomover
+    
+    
+    // Use this for initialization
+    void Start () {
         Controller = gameObject.GetComponent<CharacterController>();
         SeenFriend = false;
         CurrentSpeed = Speed;
@@ -29,16 +30,17 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        float Teste = GetPercentageMoved();
+        //Restringe a posição do jogador no espaço designado
         gameObject.transform.position = new Vector3(Mathf.Clamp(gameObject.transform.position.x, -Xlimite, Xlimite), gameObject.transform.position.y, Mathf.Clamp(gameObject.transform.position.z, -Zlimite, Zlimite));
+        //Faz a câmera acompanhar o jogador no eixo Z(longitudinal)
         Camera.transform.position = new Vector3(Camera.transform.position.x, Camera.transform.position.y, gameObject.transform.position.z);
         if(!SeenFriend)
         {
             Collider[] ColliderList;
             ColliderList = Physics.OverlapSphere(gameObject.transform.position, Range);
-            foreach (Collider element in ColliderList)
+            foreach (Collider element in ColliderList)                  //Procura por um item usando uma esfera de raio Range
             {
-                if (element.gameObject.tag == "Friend")
+                if (element.gameObject.tag == "Friend")                 //Caso encontre Inicie os efeitos no jogador
                 {
                     IniAngle = Light.GetComponent<Light>().spotAngle;
                     SeenFriend = true;
@@ -62,7 +64,7 @@ public class PlayerController : MonoBehaviour {
         Controller.Move(gameObject.transform.right * CurrentSpeed * Time.deltaTime * Input.GetAxis("Horizontal"));
     }
 
-    public float GetPercentageMoved()
+    public float GetPercentageMoved()    //Função que retorna a porcentagem de deslocamento do jogador em relação ao mapa
     {
         PercentageMoved = (gameObject.transform.position.z + Zlimite) / (2 * Zlimite) *100;
         return PercentageMoved;
--- Assets/Programs/SoundController.cs
+++ Assets/Programs/SoundController.cs
@@ -4,37 +4,45 @@ using UnityEngine;
 
 public class SoundController : MonoBehaviour {
 
+    //Variáveis para reproduzir effeitos sonoros
+    public GameObject EffectsPlayer;
+    public AudioClip Effect0;
+    public AudioClip Effect1;
+
+    //Variáveis para reproduzir sons ambiente
     public AudioClip Track0;
     public AudioClip Track1;
     public AudioClip Track2;
     public AudioClip Track3;
     private AudioSource AudioSource;
-    private GameObject Player;
+    
+    private GameObject Player;      //O jogador
 
 
     void Start () {
         AudioSource = gameObject.GetComponent<AudioSource>();
         Player = GameObject.FindWithTag("Player");
+        AudioSource.priority = 0;   //Prioridade do som de fundo ao máximo
     }
 	
 	// Update is called once per frame
 	void Update () {
-		if(Player.GetComponent<PlayerController>().GetPercentageMoved()<= 25 && !AudioSource.isPlaying)
+		if(Player.GetComponent<PlayerController>().GetPercentageMoved()<= 25 && !AudioSource.isPlaying)         //Dependendo da porcentagem de caminho deslocado altera qual música entra em loop
         {
             AudioSource.clip = Track0;
             AudioSource.Play();
         }
-        else if(Player.GetComponent<PlayerController>().GetPercentageMoved() <= 50 && !AudioSource.isPlaying)
+        else if(Player.GetComponent<PlayerController>().GetPercentageMoved() <= 50 && !AudioSource.isPlaying)   //50%
         {
             AudioSource.clip = Track1;
             AudioSource.Play();
         }
-        else if (Player.GetComponent<PlayerController>().GetPercentageMoved() <= 75 && !AudioSource.isPlaying)
+        else if (Player.GetComponent<PlayerController>().GetPercentageMoved() <= 75 && !AudioSource.isPlaying)  //75%
         {
             AudioSource.clip = Track2;
             AudioSource.Play();
         }
-        else if (!AudioSource.isPlaying)
+        else if (!AudioSource.isPlaying)                                                                        //100%
         {
             AudioSource.clip = Track3;
             AudioSource.Play();
