7e527961fe7bd684bb7f629f4a9776966ebd82c9
Lucas Nascimento Camolezi <camolezi@usp.br>
2018-09-28 21:06:02 -0300

    Mudança geral na estética e adicionado particulas

---
+++ Assets/DestuirLuz.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestuirLuz : MonoBehaviour {
+
+    private bool JaBranco = false;
+
+    public void setarBrancoTrue() {
+        JaBranco = true;
+    }
+
+    public void setarBrancoFalse() {
+        JaBranco = false;
+    }
+
+    public bool getJaBranco() {
+        return JaBranco;
+    }
+
+
+}
